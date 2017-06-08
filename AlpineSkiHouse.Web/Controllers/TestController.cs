e5e56461cc538904358150c7ad912a76e371e45e
Adrian Sanchez <me.aasanchez@gmail.com>
2017-06-08 01:58:45 -0500

    Added TestController

---
+++ AlpineSkiHouse.Web/Controllers/TestController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AlpineSkiHouse.Web.Controllers
+{
+    [Route("api/[controller]")]
+    public class TestController : Controller
+    {
+        // GET api/values
+        [HttpGet]
+        public JsonResult Get()
+        {
+            dynamic result = new ExpandoObject();
+            result.FirstName = "Adrian";
+            result.LastName = "Sanchez";
+            return new JsonResult(result);
+        }
+    }
+}
